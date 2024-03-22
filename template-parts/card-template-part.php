8058721dc70fe6ae65b75c5aa7d9176c809f6685
elisabettac77 <146635386+elisabettac77@users.noreply.github.com>
2024-03-22 11:28:53 +0100

    Update card-template-part.php
    reworked the template to avoid duplicating loop call in archive.php

--- template-parts/card-template-part.php
+++ template-parts/card-template-part.php
@@ -9,22 +9,6 @@
  */
 
 ?>
-
-<?php
-
-$args = array(
-    'post_type' => 'post', // Fetching posts
-    'posts_per_page' => 1, // Number of posts to display
-    'orderby' => 'date', // Order by date
-    'order' => 'DESC' // Latest posts first
-);
-
-$posts_query = new WP_Query($args);
-
-if ($posts_query->have_posts()) :
-    while ($posts_query->have_posts()) :
-        $posts_query->the_post();
-?>
         <div class="card">
             <div class="card-image">
                 <?php if (has_post_thumbnail()) : ?>
@@ -53,8 +37,3 @@ if ($posts_query->have_posts()) :
             <div class="card-footer">
             </div>
         </div>
-<?php
-    endwhile;
-endif;
-wp_reset_postdata(); // Reset post data query
-?>
