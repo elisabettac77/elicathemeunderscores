34800d9967fcb8ea2634de8ac8932ba4248c04d1
elisabettac77 <146635386+elisabettac77@users.noreply.github.com>
2024-03-28 09:21:02 +0100

    Update single.php
    add check for post formats, handling of exceptions (when no post format is specified)

--- single.php
+++ single.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all single posts
  *
@@ -10,30 +11,38 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+	<?php
+	if (is_singular()) {
+		$format = get_post_format();
 
-			get_template_part( 'template-parts/content', get_post_type() );
+		if ($format) {
+			$template = locate_template(array("content-$format.php"));
+			if (!$template) {
+				$template = locate_template(array('single.php'));  // Fallback to single.php
+			}
+			include($template);
+		} else {
+			// Handle posts without a format (optional)
+			include(locate_template(array('single.php')));
+		}
+	}
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'elicathemeunderscores' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'elicathemeunderscores' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+	the_post_navigation(
+		array(
+			'prev_text' => '<span class="nav-subtitle">' . esc_html__('Previous:', 'elicathemeunderscores') . '</span> <span class="nav-title">%title</span>',
+			'next_text' => '<span class="nav-subtitle">' . esc_html__('Next:', 'elicathemeunderscores') . '</span> <span class="nav-title">%title</span>',
+		)
+	);
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+	// If comments are open or we have at least one comment, load up the comment template.
+	if (comments_open() || get_comments_number()) :
+		comments_template();
+	endif;
+	?>
 
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
+</main><!-- #main -->
 
 <?php
 get_sidebar();
