52cc2ea84a225cb7f3122a187c476b520bd37b44
elisabettac77 <146635386+elisabettac77@users.noreply.github.com>
2024-04-03 17:32:57 +0200

    Update functions.php
    Removed function for HTML5 support and function for plugin activation check to resolve warnings and fatal error while trying to deactivate other plugins.
    there is no need to check if plugin is active because the theme does not require it strictly to work.

--- functions.php
+++ functions.php
@@ -8,11 +8,6 @@
  * @package elicathemeunderscores
  */
 
-function is_elicautilities_active()
-{
-	return is_plugin_active('elicautilities/elicautilities.php');
-}
-
 if (!defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
 	define('_S_VERSION', '1.0.0');
@@ -57,23 +52,6 @@ function elicathemeunderscores_setup()
 	register_nav_menus(
 		array(
 			'menu-1' => esc_html__('Primary', 'elicathemeunderscores'),
-		)
-	);
-
-	/*
-		* Switch default core markup for search form, comment form, and comments
-		* to output valid HTML5.
-		*/
-	add_theme_support(
-		'html5',
-		array(
-			'search-form',
-			'comment-form',
-			'comment-list',
-			'gallery',
-			'caption',
-			'style',
-			'script',
 		)
 	);
 
@@ -184,9 +162,4 @@ require get_template_directory() . '/inc/customizer.php';
  */
 if (defined('JETPACK__VERSION')) {
 	require get_template_directory() . '/inc/jetpack.php';
-}
-
-if (is_elicautilities_active()) {
-	require_once(ABSPATH . 'wp-content/plugins/elicautilities/elicautilities.php');
-	// ... include additional plugin files as needed
 }
